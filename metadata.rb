e8f5eb84c2d5bcceec10e29ec15d68e89d467ebb
Cédric Félizard <cedric@felizard.fr>
2014-09-13 05:57:32 +0000

    Add supported platforms

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,9 @@ license          'MIT'
 description      'Installs/Configures the official Bitcoin Core'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'redhat'
+supports 'ubuntu'
