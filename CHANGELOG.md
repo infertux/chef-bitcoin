57426868caf3df19a1209a5cda37b1dfba7b242a
Cédric Félizard <cedric@felizard.fr>
2014-09-13 06:47:54 +0000

    Bump to 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 Bitcoin CHANGELOG
 =================
 
-1.0.0
------
+## v1.0.1
+- Code formatting and various tweaks. No functional changes.
+
+## v1.0.0
 - Initial release
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cedric@felizard.fr'
 license          'MIT'
 description      'Installs/Configures the official Bitcoin Core'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'centos'
 supports 'debian'
