5a4db3586293593774d5ff50b198929ef416a3e6
Cédric Félizard <cedric@felizard.fr>
2014-09-13 15:34:17 +0000

    Rename default recipe to 'binary'

---
+++ recipes/binary.rb
@@ -0,0 +1,73 @@
+#
+# Cookbook Name:: bitcoin
+# Recipe:: binary
+#
+
+user node['bitcoin']['user'] do
+  home node['bitcoin']['binary']['home']
+  shell "/bin/bash"
+  supports manage_home: true
+end
+
+directory "#{node['bitcoin']['binary']['home']}/.bitcoin" do
+  owner node['bitcoin']['user']
+  group node['bitcoin']['user']
+  mode "0700"
+end
+
+template "#{node['bitcoin']['binary']['home']}/.bitcoin/bitcoin.conf" do
+  owner node['bitcoin']['user']
+  group node['bitcoin']['user']
+  mode "0600"
+  action :create_if_missing
+end
+
+template "#{node['bitcoin']['binary']['home']}/#{node['bitcoin']['service_wrapper']}" do
+  source "bitcoind.erb"
+  owner node['bitcoin']['user']
+  group node['bitcoin']['user']
+  mode "0500"
+end
+
+
+archive_file = "bitcoin-#{node['bitcoin']['binary']['version']}-linux.tar.gz"
+archive_path = "#{Chef::Config['file_cache_path']}/bitcoin/#{archive_file}"
+extract_path = "#{Chef::Config['file_cache_path']}/bitcoin/bitcoin-#{node['bitcoin']['binary']['version']}"
+binary_path  = "#{node['bitcoin']['home']}/#{node['bitcoin']['binary']['bitcoind']}"
+
+remote_file archive_path do
+  source "https://bitcoin.org/bin/#{node['bitcoin']['binary']['version']}/#{archive_file}"
+  owner node['bitcoin']['user']
+  group node['bitcoin']['user']
+  mode "0400"
+  not_if { ::File.exist?(archive_path) }
+end
+
+execute "verify checksum" do
+  cwd ::File.dirname(archive_path)
+  command %(echo "#{node['bitcoin']['binary']['checksum']}  #{archive_file}" | sha256sum -c -)
+end
+
+directory ::File.dirname(binary_path) do
+  owner node['bitcoin']['user']
+  group node['bitcoin']['user']
+  mode "0700"
+  recursive true
+end
+
+bash "extract archive" do
+  cwd ::File.dirname(archive_path)
+  code <<-EOH
+    rm -rf #{extract_path}
+    mkdir -p #{extract_path}
+    tar xf #{archive_file} -C #{extract_path}
+    cp #{extract_path}/*/bin/64/bitcoind #{binary_path} # FIXME: don't hard-code "64"
+  EOH
+end
+
+file binary_path do
+  owner node['bitcoin']['user']
+  group node['bitcoin']['user']
+  mode "0500"
+end
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,41 +3,5 @@
 # Recipe:: default
 #
 
-%w(start stop restart status).each do |service_action|
+raise "bitcoin::default is a NOOP"
 
-  execute "bitcoind #{service_action}" do
-    cwd node['bitcoin']['home']
-    command %(su - #{node['bitcoin']['user']} -c './#{node['bitcoin']['service_wrapper']} #{service_action}')
-    action :nothing
-  end
-
-end
-
-user node['bitcoin']['user'] do
-  home node['bitcoin']['home']
-  shell "/bin/bash"
-  supports manage_home: true
-end
-
-directory "#{node['bitcoin']['home']}/.bitcoin" do
-  owner node['bitcoin']['user']
-  group node['bitcoin']['user']
-  mode "0700"
-end
-
-template "#{node['bitcoin']['home']}/.bitcoin/bitcoin.conf" do
-  owner node['bitcoin']['user']
-  group node['bitcoin']['user']
-  mode "0400"
-  action :create_if_missing
-  notifies :run, "execute[bitcoind start]", :delayed
-end
-
-template "#{node['bitcoin']['home']}/#{node['bitcoin']['service_wrapper']}" do
-  source "bitcoind.erb"
-  owner node['bitcoin']['user']
-  group node['bitcoin']['user']
-  mode "0500"
-end
-
-include_recipe "bitcoin::install" unless ::File.exist? "#{node['bitcoin']['home']}/#{node['bitcoin']['binary']}"
