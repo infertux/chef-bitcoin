669f728ce34042d45a3e9d609bdbed33bff3ac4e
Federico Castagnini <fcastagnini@bloomberg.net>
2015-05-01 08:39:00 -0400

    scalling cores automatically

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,7 +35,7 @@ default['bitcoin']['source']['prefix'] = "/usr/local"
 default['bitcoin']['source']['bitcoind'] = "#{node['bitcoin']['source']['prefix']}/bin/bitcoind"
 default['bitcoin']['source']['bitcoin-cli'] = "#{node['bitcoin']['source']['prefix']}/bin/bitcoin-cli"
 default['bitcoin']['source']['configure_options'] = "--disable-wallet --without-gui --prefix=#{node['bitcoin']['source']['prefix']}"
-default['bitcoin']['source']['make_options'] = "-j4"
+default['bitcoin']['source']['make_options'] = "-j#{node['cpu']['total']}"
 default['bitcoin']['source']['dependencies'] = [
   'build-essential', 'autoconf', 'libssl-dev', 'libboost-dev', 'pkg-config',
   'libboost-chrono-dev', 'libboost-filesystem-dev', 'libboost-program-options-dev',
