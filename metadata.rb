932e0c8827c4a56017f3bee2cfade10bf47f6cde
Cédric Félizard <cedric@felizard.fr>
2017-01-09 19:48:32 +0000

    Add URLs to metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer       'Cédric Félizard'
 maintainer_email 'cedric@felizard.fr'
 license          'MIT'
 description      'Installs and configures Bitcoin'
+source_url       'https://github.com/infertux/chef-bitcoin'
+issues_url       'https://github.com/infertux/chef-bitcoin/issues'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.0'
 
